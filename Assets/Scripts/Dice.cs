07351cef843e739ade90351ed8e6be85ac57124f
jjaaww99 <9171002@gmail.com>
2024-08-12 17:46:42 +0900

    주사위 코루틴 제거 DOTweening 적용

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -12,50 +12,53 @@ public class Dice : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private WaitForSeconds waitForDice;
     [SerializeField] private Vector2 targetOffset;
+    [SerializeField] private Vector3 targetRotation;
     [SerializeField] private float duration;
-    [SerializeField] private Vector2 diceMove;
-
+    [SerializeField] private bool isRolling = false;
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         animator = GetComponent<Animator>();
         button = GetComponent<Button>();
         button.onClick.AddListener(RollDice);
-        waitForDice = new WaitForSeconds(0.05f);
-        diceMove = new Vector2(0, 40);
+        targetRotation = new Vector3(0, 0, 720);
     }
 
     public void RollDice()
     {
-        int rndValue = Random.Range(1, 7);
-        Debug.Log(rndValue);
+        if (!isRolling) 
+        { 
+            isRolling = true;
 
-        switch (rndValue)
-        {
-            case 1:
-                animator.SetTrigger("Dice1");
-                break;
-            case 2:
-                animator.SetTrigger("Dice2");
-                break;
-            case 3:
-                animator.SetTrigger("Dice3");
-                break;
-            case 4:
-                animator.SetTrigger("Dice4");
-                break;
-            case 5:
-                animator.SetTrigger("Dice5");
-                break;
-            case 6:
-                animator.SetTrigger("Dice6");
-                break;
-            default:
-                animator.SetTrigger("Dice1"); 
-                break;
+            int rndValue = Random.Range(1, 7);
+
+            switch (rndValue)
+            {
+                case 1:
+                    animator.SetTrigger("Dice1");
+                    break;
+                case 2:
+                    animator.SetTrigger("Dice2");
+                    break;
+                case 3:
+                    animator.SetTrigger("Dice3");
+                    break;
+                case 4:
+                    animator.SetTrigger("Dice4");
+                    break;
+                case 5:
+                    animator.SetTrigger("Dice5");
+                    break;
+                case 6:
+                    animator.SetTrigger("Dice6");
+                    break;
+                default:
+                    animator.SetTrigger("Dice1"); 
+                    break;
+            }
+
+            AnimateDice();
         }
-
-        AnimateDice();
     }
 
     private void AnimateDice()
@@ -65,60 +68,12 @@ public class Dice : MonoBehaviour
 
         Sequence sequence = DOTween.Sequence();
 
-        sequence.Append(rectTransform.DOAnchorPos(targetPosition, duration)
-                                     .SetEase(Ease.InOutQuad));
+        sequence.Append(rectTransform.DOAnchorPos(targetPosition, duration).SetEase(Ease.InOutQuad).OnComplete(() => 
+                                     rectTransform.DOAnchorPos(originalPosition, duration).SetEase(Ease.InOutQuad)));
 
-        sequence.Join(rectTransform.DORotate(new Vector3(0, 0, 720), duration * 2, RotateMode.LocalAxisAdd)
-                                 .SetEase(Ease.InOutQuad));
+        sequence.Join(rectTransform.DORotate(targetRotation, duration * 2 - 0.05f, RotateMode.LocalAxisAdd).SetEase(Ease.Linear));
 
-        sequence.Append(rectTransform.DOAnchorPos(originalPosition, duration)
-                                     .SetEase(Ease.InOutQuad));
-
-        sequence.Join(rectTransform.DORotate(Vector3.zero, 0.01f, RotateMode.FastBeyond360)
-                             .SetEase(Ease.InOutQuad));
-    }
-
-    IEnumerator RollAnimation()
-    {
-        Vector3 originalPosition = rectTransform.anchoredPosition;
-
-        Quaternion newRotation = Quaternion.identity;
-
-        yield return waitForDice;
-
-        rectTransform.anchoredPosition += diceMove;
-        newRotation = Quaternion.Euler(0f, 0f, 120f);
-        transform.rotation = newRotation;
-
-        yield return waitForDice;
-
-        rectTransform.anchoredPosition += diceMove;
-        newRotation = Quaternion.Euler(0f, 0f, 240f);
-        transform.rotation = newRotation;
-
-        yield return waitForDice;
-
-        rectTransform.anchoredPosition += diceMove;
-        newRotation = Quaternion.Euler(0f, 0f, 360f);
-        transform.rotation = newRotation;
-
-        yield return waitForDice;
-
-        rectTransform.anchoredPosition -= diceMove;
-        newRotation = Quaternion.Euler(0f, 0f, 120f);
-        transform.rotation = newRotation;
-
-        yield return waitForDice;
-
-        rectTransform.anchoredPosition -= diceMove;
-        newRotation = Quaternion.Euler(0f, 0f, 240f);
-        transform.rotation = newRotation;
-
-        yield return waitForDice;
-
-        rectTransform.anchoredPosition -= diceMove;
-        newRotation = Quaternion.Euler(0f, 0f, 360f);
-        transform.rotation = newRotation;
-
+        sequence.Append(rectTransform.DORotate(Vector3.zero, 0.2f, RotateMode.FastBeyond360) .SetEase(Ease.InOutQuad).OnComplete(() =>
+                                    isRolling = false ));
     }
 }
